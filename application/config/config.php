a07a1b2911c5aab05e75e981f1403c938332fd52
Jason Fletcher <j.fletcher@eoniantechnology.com>
2013-01-16 10:43:25 -0500

    -Change base URL for live version. -Remove unrelated testing files

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= (ENVIRONMENT == 'devel') ? 'http://links.local/' : 'http://links.ziffen.com/';
+$config['base_url']	= (ENVIRONMENT == 'devel') ? 'http://links.local/' : 'http://tools.ziffen.com/';
 
 /*
 |--------------------------------------------------------------------------
