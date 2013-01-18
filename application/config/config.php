cffa1fa2eeafef5b3cdba5ece09e89ef1e813dd6
Jason Fletcher <j.fletcher@eoniantechnology.com>
2013-01-18 04:48:00 -0500

    -Use subdirectory not subdomain on live -Add separate htaccess for live since subdirectory is not as easily supported.

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= (ENVIRONMENT == 'devel') ? 'http://links.local/' : 'https://tools.ziffen.com/';
+$config['base_url']	= (ENVIRONMENT == 'devel') ? 'http://links.local/' : 'https://www.ziffen.com/tools/';
 
 /*
 |--------------------------------------------------------------------------
